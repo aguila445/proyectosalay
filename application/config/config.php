d7f986b5fc6d463d9f985e4b239c2662e183c940
unknown <aguilar.deiby.445@gmail.com>
2023-09-13 01:15:37 -0400

    controlador usuario

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-/*$config['base_url'] = 'http://localhost:8080/proyecto/';*/
-$config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
-$config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/';
+$config['base_url'] = 'http://localhost/proyectoSalay/';
+//$config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
+//$config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/';
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -388,7 +388,7 @@ $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = APPPATH. 'session';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
--- application/controllers/usuarios.php
+++ application/controllers/usuarios.php
@@ -1,4 +1,3 @@
-<?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Usuarios extends CI_Controller
@@ -12,10 +11,14 @@ class Usuarios extends CI_Controller
             $this->load->view('login', $data);
         }
     }
+
     public function validarusuario()
     {
         $login = $_POST['login'];
         $password = md5($_POST['password']);
+
+        // Aquí debes implementar la lógica para validar al usuario
+        // Utiliza los modelos y funciones correspondientes para verificar las credenciales
 
         $consulta = $this->usuario_model->validar($login, $password);
 
@@ -23,30 +26,43 @@ class Usuarios extends CI_Controller
             foreach ($consulta->result() as $row) {
                 $this->session->set_userdata('idusuario', $row->idUsuario);
                 $this->session->set_userdata('login', $row->login);
-                $this->session->set_userdata('tipo', $row->tipo);
-                redirect('usuarios/panel', 'refresh');
+                $this->session->set_userdata('tipo', $row->rol);
+
+                // Redirige según el rol del usuario (administrador, encargado, guía, fraterno)
+                if ($row->rol == 'Administrador') {
+                    redirect('admin/panel', 'refresh');
+                } elseif ($row->rol == 'Encargado') {
+                    redirect('encargado/panel', 'refresh');
+                } elseif ($row->rol == 'Guía') {
+                    redirect('guia/panel', 'refresh');
+                } elseif ($row->rol == 'Fraterno') {
+                    redirect('fraterno/panel', 'refresh');
+                }
             }
         } else {
-            redirect('usuarios/index/1', 'refresh');
+            redirect('usuarios/index/1', 'refresh'); // Error de inicio de sesión
         }
     }
+
     public function panel()
     {
         if ($this->session->userdata('login')) {
-            $tipo=$this->session->userdata('tipo');
-            if ($tipo=='admin') {
-                redirect('estudiante/indexlte', 'refresh');
-            }
-            else {
-                redirect('estudiante/invitadolte', 'refresh');
+            $tipo = $this->session->userdata('tipo');
+
+            // Redirige según el rol del usuario
+            if ($tipo == 'Administrador') {
+                redirect('admin/panel', 'refresh');
+            } else {
+                redirect('usuario/panel', 'refresh');
             }
         } else {
-            redirect('usuarios/index/2', 'refresh');
+            redirect('usuarios/index/2', 'refresh'); // Usuario no autenticado
         }
     }
+
     public function logout()
     {
         $this->session->sess_destroy();
-        redirect('usuarios/index/3', 'refresh');
+        redirect('usuarios/index/3', 'refresh'); // Usuario cerró sesión
     }
 }
